95707ec31c21cd3a94ae9210f72750ee4e0a6dc1
Mobin Hosseini <kaskop69alt@gmail.com>
2025-08-31 02:39:36 +0100

    post processing added (bloom)

--- src/app/components/home3D.tsx
+++ src/app/components/home3D.tsx
@@ -4,6 +4,9 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js"
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js"
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 export default function ThreeText() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -34,7 +37,21 @@ export default function ThreeText() {
     camera.position.z = 0;
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const composer = new EffectComposer(renderer);
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
+    const bloomPass = new UnrealBloomPass(
+      new THREE.Vector2(mountRef.current.clientWidth, mountRef.current.clientHeight),
+      0.3, // strength
+      0.4,
+      1
+    );
+
+    composer.addPass(bloomPass);
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
+    composer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
     mountRef.current.appendChild(renderer.domElement);
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
@@ -51,8 +68,8 @@ export default function ThreeText() {
     scene.add(fillLight);
 
     // Rim/back light
-    const rimLight = new THREE.DirectionalLight(0x88ccff, 0.7); // bluish tint
-    rimLight.position.set(0, 5, -10);
+    const rimLight = new THREE.DirectionalLight(0x88ccff, 1); // bluish tint
+    rimLight.position.set(0, 5, -30);
     scene.add(rimLight);
 
     // Hemisphere light
@@ -60,15 +77,19 @@ export default function ThreeText() {
     scene.add(hemiLight);
 
     const particlesGeometry = new THREE.BufferGeometry();
-    const particlesCount = 900;
+    const particlesCount = 1100;
     const positionBuffer = new Float32Array(particlesCount * 3);
     const colorBuffer = new Float32Array(particlesCount * 4);
     const particleSpeeds = new Float32Array(particlesCount);
 
-    for(let i = 0; i < particlesCount; i++) {
-      positionBuffer[i * 3] = (Math.random() - 0.5) * 100; // X
-      positionBuffer[i * 3 + 1] = (Math.random() - 0.5) * 100; // Y
-      positionBuffer[i * 3 + 2] = (Math.random() * -100) + 50; // Z
+    const visibleArea = getFrustumEdgesAtZ(camera, -25);
+
+    for (let i = 0; i < particlesCount; i++) {
+      positionBuffer[i * 3] = (Math.random() * (visibleArea.right - visibleArea.left) * 1.2) 
+        + visibleArea.left * 1.2; // X
+      positionBuffer[i * 3 + 1] = (Math.random() * (visibleArea.top - visibleArea.bottom) * 1.2) 
+        + visibleArea.bottom * 1.2; // Y
+      positionBuffer[i * 3 + 2] = (Math.random() * -50); // Z
       particleSpeeds[i] = (Math.random() * 0.1) + 0.05; // Z Speed
       colorBuffer[i * 4] = 255; // R
       colorBuffer[i * 4 + 1] = 255; // G
@@ -81,13 +102,14 @@ export default function ThreeText() {
 
     const particlesMaterial = new THREE.PointsMaterial({
         size: 0.08,
-        color: 0xCCCCCC,
+        color: 0x888888,
         transparent: true,
-        opacity: 1,
+        opacity: 0.7,
         vertexColors: true,
         depthWrite: false,
-        blending: THREE.AdditiveBlending,
     });
+
+    
 
     const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
     scene.add(particlesMesh);
@@ -97,19 +119,26 @@ export default function ThreeText() {
     loader.load("/three_fonts/Lexend_Giga_Regular.json", (font) => {
       const textGeometry = new TextGeometry("mobinh.dev", {
         font: font,
-		size: 3,
-		depth: 0.5,
-		curveSegments: 12,
+        size: 3,
+        depth: 0.5,
+        curveSegments: 12,
         bevelEnabled: true,
-		bevelThickness: 0.2,
-		bevelSize: 0.05,
-		bevelOffset: 0,
-		bevelSegments: 5
+        bevelThickness: 0.2,
+        bevelSize: 0.05,
+        bevelOffset: 0,
+        bevelSegments: 5
       });
 
       textGeometry.center();
 
-      const material = new THREE.MeshStandardMaterial({ color: 0x666666, metalness: 0.6, roughness: 0.4 });
+      const material = new THREE.MeshStandardMaterial({ 
+        color: 0x666666, 
+        metalness: 0.6, 
+        roughness: 0.4,
+        emissive: 0xff4400,
+        emissiveIntensity: 0.4
+      });
+
       const textMesh = new THREE.Mesh(textGeometry, material);
       textMesh.position.z = -25;
 
@@ -139,7 +168,7 @@ export default function ThreeText() {
         particlesGeometry.attributes.position.needsUpdate = true;
         particlesGeometry.attributes.color.needsUpdate = true;
 
-        renderer.render(scene, camera);
+        composer.render();
       }
 
       animate();
@@ -148,9 +177,11 @@ export default function ThreeText() {
     // Handle window resize
     const handleResize = () => {
       if (!mountRef.current) return;
+
       camera.aspect = mountRef.current.clientWidth / mountRef.current.clientHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
+      composer.setSize(mountRef.current.clientWidth, mountRef.current.clientHeight);
     };
 
     window.addEventListener('resize', handleResize);
